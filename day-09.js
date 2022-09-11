502e2db6a473c77ba3de02bf53845406ee169096
YPK <90822823+flusnuk@users.noreply.github.com>
2022-09-11 23:21:55 +0300

    Create day-09.js

---
+++ day-09.js
@@ -0,0 +1,48 @@
+// https://www.codewars.com/kata/57280481e8118511f7000ffa/train/javascript
+
+function splitAndMerge(string, separator) {
+  let splitStr = string.split("");
+  let res = "";
+  for (let i = 0; i < splitStr.length; i++) {
+    if (splitStr[i + 1] === undefined) {
+      res += splitStr[i];
+    } else if (splitStr[i] !== " " && splitStr[i + 1] !== " ") {
+      res += splitStr[i] + separator;
+    } else {
+      res += splitStr[i];
+    }
+  }
+  return res;
+}
+
+
+// https://www.codewars.com/kata/57277a31e5e51450a4000010
+
+function firstToLast(str, c) {
+  let lastIn;
+  let frstIn;
+  if (str.includes(c)) {
+    frstIn = str.indexOf(c);
+    lastIn = str.lastIndexOf(c);
+    return lastIn - frstIn;
+  } else {
+    return -1;
+  }
+}
+
+
+// https://www.codewars.com/kata/57274562c8dcebe77e001012
+
+function cutIt(arr) {
+  let size = arr[0].length;
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length <= size) {
+      size = arr[i].length;
+    }
+  }
+  for (let v = 0; v < arr.length; v++) {
+    res.push(arr[v].slice(0, size));
+  }
+  return res;
+}
