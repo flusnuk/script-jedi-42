7b025ac6bb1470c2190da11c10ba22bde4492351
YPK <90822823+flusnuk@users.noreply.github.com>
2022-11-18 12:51:56 +0200

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/573bca07dffc1aa693000139
+let regex=/^-?9\d*0{4}$/
+
+// https://www.codewars.com/kata/573d11c48b97c0ad970002d4
+let regex=/https?:\/\/[a-z\d.]+\.(com|net)/gi;
