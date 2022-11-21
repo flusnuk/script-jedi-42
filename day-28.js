44ba5325ee403e6cb1056f42b2069239044a2ed0
YPK <90822823+flusnuk@users.noreply.github.com>
2022-11-21 21:17:16 +0200

    Create day-28.js

---
+++ day-28.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/a-function-within-a-function
+
+function always (n) {
+  return function () {
+      return n;
+    }
+}
+
+// https://www.codewars.com/kata/59bdbe9d46038724ca0000b9/train/javascript
