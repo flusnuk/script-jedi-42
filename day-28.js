65cf9b66c3624a45e25ecebc048d499fa62ba5a0
YPK <90822823+flusnuk@users.noreply.github.com>
2022-11-22 01:08:27 +0200

    Update day-28.js

--- day-28.js
+++ day-28.js
@@ -7,3 +7,7 @@ function always (n) {
 }
 
 // https://www.codewars.com/kata/59bdbe9d46038724ca0000b9/train/javascript
+
+function solution(arr, options) {
+  return arr.map(el => el += (options.modifier * 2));
+}
