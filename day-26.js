5c573bb3749866556ef8f1af08243de6c1bf73a0
YPK <90822823+flusnuk@users.noreply.github.com>
2022-11-23 00:21:57 +0200

    Create day-26.js

---
+++ day-26.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/sort-with-arrow-functions
+
+let OrderPeople = function (people) {
+ return people.sort((a, b) => a.age - b.age);
+};
+
+// https://www.codewars.com/kata/52597aa56021e91c93000cb0/
